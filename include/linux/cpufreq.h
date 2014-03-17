808760f1148e1e038a896fa49f7b80f761b0b0b8
xiangxin <xiangxin19960319@gmail.com>
2014-03-18 01:27:24 +0800

    add hotplug-governors

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -375,6 +375,9 @@ extern struct cpufreq_governor cpufreq_gov_minmax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass2)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HOTPLUG)
+extern struct cpufreq_governor cpufreq_gov_hotplug;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hotplug)
 #endif
 
 

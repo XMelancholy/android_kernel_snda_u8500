61e2cb6f91f11d3ee7475f536c1c8ce48f73ba43
xiangxin <xiangxin19960319@gmail.com>
2014-05-04 14:50:08 +0800

    Fix power_off error

--- kernel/sys.c
+++ kernel/sys.c
@@ -434,7 +434,7 @@ void kernel_power_off(void)
 	kernel_shutdown_prepare(SYSTEM_POWER_OFF);
 	if (pm_power_off_prepare)
 		pm_power_off_prepare();
-	migrate_to_reboot_cpu();
+	disable_nonboot_cpus();
 	syscore_shutdown();
 	printk(KERN_EMERG "Power down.\n");
 	kmsg_dump(KMSG_DUMP_POWEROFF);

55f96ee2f13cbd35b3fa3cc17dd9985d0b467d86
XianGxin <xiangxin19960319@gmail.com>
2014-05-13 22:10:59 +0800

    Revert "Fix power_off error"
    This reverts commit 61e2cb6f91f11d3ee7475f536c1c8ce48f73ba43.

--- kernel/sys.c
+++ kernel/sys.c
@@ -434,7 +434,7 @@ void kernel_power_off(void)
 	kernel_shutdown_prepare(SYSTEM_POWER_OFF);
 	if (pm_power_off_prepare)
 		pm_power_off_prepare();
-	disable_nonboot_cpus();
+	migrate_to_reboot_cpu();
 	syscore_shutdown();
 	printk(KERN_EMERG "Power down.\n");
 	kmsg_dump(KMSG_DUMP_POWEROFF);
